603115aee87a85f3a1324d091abca063a59af727
naveen k <naveendbt1@gmail.com>
2024-11-05 16:54:39 +0000

    This files r committed

---
+++ models/NAVEENPSVTABLE11.sql
@@ -0,0 +1,7 @@
+select * from NAVEEN_DB.NAVEEN_SC.PSVTABLE
+union all
+select * from NAVEEN_DB.NAVEEN_SC.PSVTABLE
+union all
+select * from NAVEEN_DB.NAVEEN_SC.PSVTABLE
+union all
+select * from NAVEEN_DB.NAVEEN_SC.PSVTABLE
